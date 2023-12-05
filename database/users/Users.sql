263d638892d4554613f35db016aab7ad4d45c6d1
0xkuma <kh.cheung523@gmail.com>
2023-12-05 18:36:20 +0800

    Add user table sql

---
+++ database/users/Users.sql
@@ -0,0 +1,9 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS users (
+  user_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  username VARCHAR(255) NOT NULL,
+  e_password VARCHAR(255) NOT NULL,
+  salt VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL
+);
