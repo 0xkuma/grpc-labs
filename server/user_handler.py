8ce7be5b0f72a0207bc927866761b6f48f9bc6b8
0xkuma <kh.cheung523@gmail.com>
2023-12-07 16:03:42 +0800

    Add truncated exponential backoff function

--- server/user_handler.py
+++ server/user_handler.py
@@ -1,6 +1,8 @@
 import logging
 import string
 import re
+import random
+from time import sleep
 from db_handler import Database
 from argon2 import PasswordHasher, exceptions
 
@@ -8,7 +10,27 @@ from argon2 import PasswordHasher, exceptions
 class User():
     def __init__(self):
         self.db = Database()
-        self.retry = 0
+        self.attempts = 0
+        self.max_attempts = 3
+        self.max_backoff_time = 20
+
+    def get_truncated_exponential_backoff_time(self, attempt: int) -> int:
+        """
+        The function to calculate the delay for a retry attempt using
+        the Truncated Exponential Backoff algorithm.
+
+        Parameters:
+        attempt (int): The number of the current retry attempt.
+
+        Returns:
+        int: Amount of time to delay.
+        """
+
+        delay = min(self.max_backoff_time, 2 * 2 ** attempt)
+        jitter = random.uniform(0.5, 1.5)
+        print("Delay: ", delay, "Jitter: ", jitter, "Delay * Jitter: ",
+              delay * jitter, "Attempt: ", attempt)
+        return delay * jitter
 
     @staticmethod
     def is_validate_password(password: str) -> bool:
@@ -27,25 +49,27 @@ class User():
         return False
 
     def create_user(self, username: str, password: str, salt: str, email: str) -> str:
-        while self.retry < 3:
+        while self.attempts < self.max_attempts:
             try:
                 print("Creating user")
                 res = self.db.execute("INSERT INTO users(username, e_password, salt, email) VALUES (%s, %s, %s, %s) RETURNING user_id", (
                     username, password, salt, email))
                 print(res[0])
                 if res and res[0]:
-                    self.retry = 0
+                    self.attempts = 0
                     return res[0][0]
                 else:
                     self.db.rollback()
-                    self.retry += 1
+                    self.attempts += 1
+                    sleep(self.get_truncated_exponential_backoff_time(self.attempts))
                     continue
             except Exception as e:
                 logging.error(e)
+                self.attempts += 1
+                sleep(self.get_truncated_exponential_backoff_time(self.attempts))
                 self.db.create_connection()
-                self.retry += 1
                 continue
-        self.retry = 0
+        self.attempts = 0
         raise Exception("Failed to create user")
 
     def is_user_exists(self, username: str) -> bool:
